84895f4616e88f798716cc27ffdbc48d8efbe1ee
bheftye <bheftye92@gmail.com>
2015-04-23 12:17:20 -0500

    Ultima Base de Datos

--- mvc/database/mvc_share.sql
+++ mvc/database/mvc_share.sql
@@ -1,39 +1,33 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.2.10
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-04-2015 a las 09:38:24
--- Versión del servidor: 5.6.21
--- Versión de PHP: 5.6.3
+-- Host: localhost:8889
+-- Generation Time: Apr 23, 2015 at 07:16 PM
+-- Server version: 5.5.38
+-- PHP Version: 5.6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Base de datos: `mvc_share`
+-- Database: `mvc_share`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `friends`
+-- Table structure for table `friends`
 --
 
-CREATE TABLE IF NOT EXISTS `friends` (
+CREATE TABLE `friends` (
   `id_user` int(255) NOT NULL,
   `id_friend` int(255) NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `friends`
+-- Dumping data for table `friends`
 --
 
 INSERT INTO `friends` (`id_user`, `id_friend`, `status`) VALUES
@@ -41,23 +35,25 @@ INSERT INTO `friends` (`id_user`, `id_friend`, `status`) VALUES
 (1, 11, 1),
 (1, 10, 1),
 (1, 9, 1),
-(9, 11, 1);
+(9, 11, 1),
+(17, 11, 1),
+(17, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `post`
+-- Table structure for table `post`
 --
 
-CREATE TABLE IF NOT EXISTS `post` (
+CREATE TABLE `post` (
 `id_post` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `filepath` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `post`
+-- Dumping data for table `post`
 --
 
 INSERT INTO `post` (`id_post`, `id_user`, `description`, `filepath`) VALUES
@@ -67,24 +63,29 @@ INSERT INTO `post` (`id_post`, `id_user`, `description`, `filepath`) VALUES
 (4, 11, 'Este es un ejemplo de un mapa mental', 'ejemploMapaMental.png'),
 (5, 11, 'Ejemplo ', 'ejemplo.png'),
 (6, 8, 'Cascadas', 'watetfalls.jpg'),
-(7, 8, 'Jennifer Lawrence', '7.fw.png');
+(7, 8, 'Jennifer Lawrence', '7.fw.png'),
+(8, 11, '', '1.jpg'),
+(9, 11, '', '1.jpg'),
+(10, 11, '', 'Snoop Dogg ft The Doors - Riders on the Storm (Need For Speed Underground 2 Soundtrack) [Full HD].mp3'),
+(11, 11, '', 'MT-MatrizTrazabilidadv1.0.docx'),
+(12, 11, '', '11112933_691654510935389_6270303254465132544_n.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
 `id_user` int(100) NOT NULL,
   `username` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `name` varchar(1000) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `name`, `password`) VALUES
@@ -95,61 +96,58 @@ INSERT INTO `user` (`id_user`, `username`, `email`, `name`, `password`) VALUES
 (8, 'tianafrog', 'tiana@gmail.com', 'Tiana', '12345'),
 (9, 'karimychable', 'karimychable@gmail.com', 'Karimy ChablÃ©', '123456'),
 (10, 'mvilla', 'mvilla@gmail.com', 'Maya Villanueva', '123456'),
-(11, 'bheftye', 'bheftye@gmail.com', 'Brent Heftye', '123456');
+(11, 'bheftye', 'bheftye@gmail.com', 'Brent Heftye', 'e10adc3949ba59abbe56e057f20f883e'),
+(17, 'admin', 'no_me_lo_se@gmail.com', 'Rodrigo Esparza', '21232f297a57a5a743894a0e4a801fc3');
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `friends`
+-- Indexes for table `friends`
 --
 ALTER TABLE `friends`
  ADD KEY `id_user` (`id_user`,`id_friend`), ADD KEY `id_friend` (`id_friend`);
 
 --
--- Indices de la tabla `post`
+-- Indexes for table `post`
 --
 ALTER TABLE `post`
  ADD PRIMARY KEY (`id_post`), ADD KEY `id_user` (`id_user`);
 
 --
--- Indices de la tabla `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
  ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `post`
+-- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
--- AUTO_INCREMENT de la tabla `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id_user` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `id_user` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `friends`
+-- Constraints for table `friends`
 --
 ALTER TABLE `friends`
 ADD CONSTRAINT `id_friend_pk` FOREIGN KEY (`id_friend`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `id_user_pk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `post`
+-- Constraints for table `post`
 --
 ALTER TABLE `post`
 ADD CONSTRAINT `id_user_pk_posts` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
